9168f74912572e1490c78b9578901454e91a106a
bongjour <blingface@naver.com>
2025-04-02 14:33:39 +0900

    ww

--- frontend/src/newsApp.js
+++ frontend/src/newsApp.js
@@ -17,7 +17,7 @@ function NewsApp() {
 
   const fetchNews = async () => {
     const response = await axios.get(
-      `http://localhost:8000/api/news/?q=${query}`
+      `${process.env.REACT_APP_API_URL}/api/news/?q=${query}`
     );
     setArticles(response.data);
   };
