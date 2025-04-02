9e58b22d7f57b6f1dd94a6bcb801e901dce76515
bongjour <blingface@naver.com>
2025-04-02 14:26:29 +0900

    wd

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
 import requests
 
 class NewsSummaryView(APIView):
+    renderer_classes = [JSONRenderer] 
     def get(self, request):
         keyword = request.GET.get("q", "technology")
         url = f"https://newsapi.org/v2/everything?q={keyword}&apiKey=5ed7cecaa24949c9868af2e47f81b774"
